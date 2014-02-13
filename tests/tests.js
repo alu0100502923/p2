004329395625438e97968ab2524612482267eafd
Eliasib Garcia <alu0100698121@ull.edu.es>
2014-02-13 22:06:11 +0000

    Añadida prueba. Ejecución testeada y correcta. Añadido enlace a las pruebas desde la página principal.

--- tests/tests.js
+++ tests/tests.js
@@ -6,9 +6,14 @@ suite('csv', function() {
 		calculate();
 		assert.deepEqual(result.innerHTML, "\n<tbody><tr>\t<td>Nombre</td>\t<td>Edad</td></tr>\n<tr>\t<td>Juan</td>\t<td>30</td></tr>\n</tbody>");
 	});
-	test('Tratamiento de errores', function() {
+	test('Detección de errores', function() {
 		original.value = "\"hola\" \"mundo\"\n\"helloworld\"";
 		calculate();
 		assert.isNotNull(result.innerHTML.match(/class=\"error\"/));
+	});
+	test('Detección de acierto', function() {
+		original.value = "\"Nombre\",\"Edad\"\n\"Juan\",30";
+		calculate();
+		assert.isNull(result.innerHTML.match(/class=\"error\"/));
 	});
 });
